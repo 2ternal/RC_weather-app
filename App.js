22be24ba7acd3de76572f0c222647f24754b538c
woocheol Kwon <ddft97@gmail.com>
2021-05-31 00:14:36 +0900

    TouchableOpacity

--- App.js
+++ App.js
@@ -1,91 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default class App extends React.Component {
-  fruits = [
-    { name: 'Acai' },
-    { name: 'Apples' },
-    { name: 'Apricots' },
-    { name: 'Avocado' },
-    { name: 'Ackee' },
-    { name: 'Bananas' },
-    { name: 'Bilberries' },
-    { name: 'Blueberries' },
-    { name: 'Blackberries' },
-    { name: 'Boysenberries' },
-    { name: 'Bread fruit' },
-    { name: 'Cantaloupes (cantalope)' },
-    { name: 'Chocolate-Fruit' },
-    { name: 'Cherimoya' },
-    { name: 'Cherries' },
-    { name: 'Cranberries' },
-    { name: 'Cucumbers' },
-    { name: 'Currants' },
-    { name: 'Dates' },
-    { name: 'Durian' },
-    { name: 'Eggplant' },
-    { name: 'Elderberries' },
-    { name: 'Figs' },
-    { name: 'Gooseberries' },
-    { name: 'Grapes' },
-    { name: 'Grapefruit' },
-    { name: 'Guava' },
-    { name: 'Honeydew melons' },
-    { name: 'Horned melon (Kiwano)' },
-    { name: 'Huckleberries' },
-    { name: 'Ita Palm' },
-    { name: 'Jujubes' },
-    { name: 'Kiwis' },
-    { name: 'Durian is an unusual tropical fruit.' },
-    { name: 'Kumquat' },
-    { name: 'Lemons' },
-    { name: 'Limes' },
-    { name: 'Lychees' },
-    { name: 'Mangos' },
-    { name: 'Mangosteen' },
-    { name: 'Mulberries' },
-    { name: 'Muskmelon' },
-    { name: 'Nectarines' },
-    { name: 'Ogden melons' },
-    { name: 'Olives' },
-    { name: 'Oranges' },
-    { name: 'Papaya' },
-    { name: 'Passion fruit' },
-    { name: 'Peaches' },
-    { name: 'Pears' },
-    { name: 'Peppers' },
-    { name: 'Persimmon' },
-    { name: 'Pineapple' },
-    { name: 'Plums' },
-    { name: 'Pluot' },
-    { name: 'Pomegranate' },
-    { name: 'Prickly Pear' },
-    { name: 'Quince' },
-    { name: 'Rambuton' },
-    { name: 'Raspberries' },
-    { name: 'Rose Apple' },
-    { name: 'Starfruit' },
-    { name: 'Sapadilla' },
-    { name: 'Strawberries' },
-    { name: 'Tamarind' },
-    { name: 'Tangelo' },
-    { name: 'Tangerines' },
-    { name: 'Tomatoes' },
-    { name: 'Ugli fruit' },
-    { name: 'Voavanga (Spanish Tamarind)' },
-    { name: 'Watermelons' },
-    { name: 'Xigua melon' },
-    { name: 'Yellow watermelon' },
-    { name: 'Zucchini' },
-  ];
+  constructor(props) {
+    super(props);
 
-  renderItem({ name }) {
+    this.state = {
+      cities: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://raw.githubusercontent.com/example0312/weather-crawler/master/availableCityNames')
+      .then(response => response.json())
+      .then(cities => {
+        console.log('cities =', cities.length);
+        this.setState({
+          cities
+        });
+      });
+  }
+
+  onPressCity(item) {
+    console.log('onPressCity =', item);
+  }
+
+  renderItem(city) {
     return (
-      <View style={styles.item}>
-        <Text style={styles.text}>{name}</Text>
-      </View>
+      <TouchableOpacity style={styles.item} onPress={() => this.onPressCity(city)}>
+        <Text style={styles.text}>{city}</Text>
+      </TouchableOpacity>
     );
   }
 
@@ -93,11 +39,11 @@ export default class App extends React.Component {
     return (
       <SafeAreaView style={styles.container}>
         <FlatList style={styles.container}
-          keyExtractor={(item) => item.name}
+          keyExtractor={item => item}
           renderItem={({ item }) => this.renderItem(item)}
-          data={this.fruits}
+          data={this.state.cities}
         />
-        <StatusBar style="auto" />        
+        <StatusBar style="auto" />
       </SafeAreaView>
     );
   }
