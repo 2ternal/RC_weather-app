50f3dc5f8afebcb5e31fdbac26d1ac27b06037d7
woocheol Kwon <ddft97@gmail.com>
2021-05-30 22:50:48 +0900

    Make button

--- App.js
+++ App.js
@@ -1,14 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={[styles.textBig, styles.textRed]}>Hello, world!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class CounterButton extends React.Component {
+  static defaultProps = {
+    counter: -1,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: props.counter,
+    };
+  }
+
+  render() {
+    const clickHandler = () => {
+      this.setState({
+        counter: this.state.counter + 1,
+      });
+    };
+
+    return (
+      <View style={styles.buttonContainer}>
+          <Text>{this.state.counter}</Text>
+          <Button title={'click me!'} onPress={clickHandler}/>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+}
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <CounterButton counter={0} />
+        <CounterButton />
+        <CounterButton counter={2} />
+        <CounterButton counter={3} />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -18,10 +53,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  textBig: {
-    fontSize: 32,
-  },
-  textRed: {
-    color: 'red'
+  buttonContainer: {
+    alignItems: 'center',
+    width: '100%',
+    borderWidth: 1,
+    borderColor: 'blue',
   },
 });
